c2f08d0dce991e8eeab669f732ff6331dba5f33b
Dennis Jakobi <dennis.jakobi76@outlook.de>
2024-11-07 08:32:30 +0100

    added functionality for wrong and right answers

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
+const questionsCount = document.getElementById("all-questions-count");
+const questionCounter = document.getElementById("question-counter");
+const questionContainer = document.getElementById("questions-container");
+const answerContainer1 = document.getElementById("answer1");
+const answerContainer2 = document.getElementById("answer2");
+const answerContainer3 = document.getElementById("answer3");
+const answerContainer4 = document.getElementById("answer4");
+
 let questions = [
     {
         "question": "Wer hat HTML erfunden?",
@@ -8,7 +16,7 @@ let questions = [
         "right_answer": 3,
     },
     {
-        "question": "Was bedeutet das HTML-Tag &,t;a&gt;?",
+        "question": "Was bedeutet das HTML-Tag &lt;a&gt;?",
         "answer_1": "Text Fett",
         "answer_2": "Container",
         "answer_3": "Ein Link",
@@ -48,3 +56,72 @@ let questions = [
         "right_answer": 4,
     },
 ];
+
+let currentQuestion = 0;
+let question = questions[currentQuestion];
+
+function init() {
+    questionsCount.innerHTML = questions.length;
+    showQuestion();
+}
+
+function showQuestion() {
+    questionCounter.innerHTML = currentQuestion + 1;
+    question = questions[currentQuestion];
+    questionContainer.innerHTML = question.question;
+    answerContainer1.innerHTML = question.answer_1;
+    answerContainer2.innerHTML = question.answer_2;
+    answerContainer3.innerHTML = question.answer_3;
+    answerContainer4.innerHTML = question.answer_4;
+}
+
+function answer(answerText) {
+    question = questions[currentQuestion];
+    let correct = answerText.includes(question.right_answer);
+
+    if (correct) {
+        document.getElementById(`ans-card-${question.right_answer}`).classList.add("correct-answer");
+    } else {
+        document.getElementById(`ans-card-${answerText.slice(-1)}`).classList.add("wrong-answer");
+    }
+}
+
+function lastQuestion() {
+    resetAnswers();
+    if (currentQuestion == 0) {
+        currentQuestion = questions.length - 1;
+    } else {
+        currentQuestion--;
+    }
+
+    showQuestion();
+}
+
+function nextQuestion() {
+    resetAnswers();
+    if (currentQuestion == questions.length - 1) {
+        currentQuestion = 0;
+    } else {
+        currentQuestion++;
+    }
+
+    showQuestion();
+}
+
+function resetAnswers() {
+    let answers = document.getElementsByClassName("selectable");
+
+    for (let i = 0; 0 < answers.length; i++) {
+        if (answers[i].classList.contains("correct-answer")) {
+            answers[i].classList.remove("correct-answer");
+        }
+
+        if (answers[i].classList.contains("wrong-answer")) {
+            answers[i].classList.remove("wrong-answer");
+        }
+
+        if (i == 3) {
+            break;
+        }
+    }
+}
